0353bb4128ce019d77385dd27cd0f41aa1ad966b
nicholas-net <cloudvibeclothing@gmail.com>
2025-07-01 23:15:27 -0400

    started console UI

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #All code written by Nicholas Colon
-from stats import get_number_of_words, get_books_text
+
+from stats import get_number_of_words, get_books_text, get_character_frequency, sort_frequency, print_interface
+
 
 def main():
-    book = "books/frankenstein.txt"
-    text = get_books_text(book)
-    get_number_of_words(text)
+    book_path = "books/frankenstein.txt"
+    text = get_books_text(book_path)
+    words = get_number_of_words(text)
+    dict = get_character_frequency(text)
+    sorted_char_count = sort_frequency(dict)
+    print(print_interface(book_path, words, sorted_char_count))
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,9 +1,8 @@
-
 def get_books_text(path_to_file) -> str:
     """
-     Opens the frankenstein book file and returns the contents of the book as a str.
+    Opens the frankenstein book file and returns the contents of the book as a str.
 
-     Args:
+    Args:
             path_to_file : relative directory location of the frankenstein file.
 
     Returns:
@@ -11,23 +10,97 @@ def get_books_text(path_to_file) -> str:
     """
 
     with open(path_to_file) as frankenstein_file:
+        # Returns all the bytes of the file as a string.
         file_contents = frankenstein_file.read()
     return file_contents
 
-def get_number_of_words(book_text):
+def get_number_of_words(book_text: str) -> int:
     """
     Splits each word from the frankenstein file into an array. Total counter represents the number of words
     in the book. Increments every loop.
 
     Args:
-        (str) : extracted frankenstein file text
+        (str) : frankenstein file text.
 
     Returns
-        int: total word count
+        int: total word count.
 
     """
     words = book_text.split()
     total=0
     for word in words:
         total+=1
-    print(f"{total} words found in the document")
+    return total
+
+def get_character_frequency(book_text: str) -> dict:
+    """
+    Count the total frequency of characters from the book. Returns characters and frequencies as
+    a key, value pair.
+
+    Args:
+        (str) : frankenstein text.
+
+    Returns:
+        dict: dictionary with every characters frequency returned.
+    """
+
+    #New dictionary to store the frequencies.
+    frequency = {}
+    #Eliminates capitalization's.
+    book_text = book_text.lower()
+    #Create a set of each unique character found to use the count function on each one.
+    for letter in set(book_text):
+        frequency[letter] = book_text.count(letter)
+
+    return frequency
+
+def sort_on(char_frequencies):
+    """
+    Lets the sort function know what key to sort the dictionaries on.
+
+    Args:
+         (dict) : dictionary of character frequencies from the frankenstein book.
+    """
+    return char_frequencies["num"]
+
+def sort_frequency(character_frequencies: dict) -> list:
+    """
+    Sort the character count in descending order.
+
+    Args:
+        (dict) : dictionary of characters and frequency.
+
+    Returns:
+        list: sorted list of dictionaries.
+    """
+
+    #Holds the character dictionaries in a list
+    sorted_dictionaries = []
+
+    #Loops through each character key and total value
+    for char, count in character_frequencies.items():
+        #Initializes a new seperate dictionary to hold each char and count pair
+        sorted_dictionaries.append({"char": char, "num": count})
+        # Sorts the list by largest count, in ascending order
+        sorted_dictionaries.sort(reverse=True,key=sort_on)
+    return sorted_dictionaries
+
+
+def _analyze_book_path(book_path):
+    return f"Analyzing book found at {book_path}..."
+
+def print_interface(book_path: str, total_words, char_frequencies):
+
+    return _analyze_book_path(book_path)
+
+
+
+
+
+
+
+
+
+
+
+
